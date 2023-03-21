3c443a729c2f59ebfd7d0cfb2cdb81cec03f91e5
joseD <josed3000@gmail.com>
2023-03-21 12:49:57 +0100

    basename BrowserRouter fixed

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className={`App background--general--${mode}`}>
         <div className='main-screen'>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home 
                                     lightmode={mode} 
